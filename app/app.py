7fb70efceff1a021203ee34c82ac1432817305dd
Steve Calnan <steve.calnan@datadoghq.com>
2024-06-12 00:06:32 -0700

    added logging

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 import http.server
 import socketserver
 import random
+import logging
 
 PORT = 5000
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def is_prime(num):
     """Check if a number is a prime number."""
@@ -27,16 +31,25 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Vanilla Py Web OK! Make me do some work at prime/")
+            logger.info(f"Served greeting")
         elif self.path == '/prime':
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             prime = get_random_prime()
             self.wfile.write(f"Here's a prime number: {prime}".encode())
+            logger.info(f"Served prime number {prime}")
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            prime = get_random_prime()
+            self.wfile.write(f"Sorry, I can't find that.".encode())
+            logger.error(f"{self.path} not found")
 
 handler = SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), handler) as httpd:
-    print(f"Serving on port {PORT}")
+    logger.info(f"Starting Vanilla Py Web Server on {PORT}")
     httpd.serve_forever()
 
